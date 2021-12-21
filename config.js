4909a06d3031217359416411d8358de3e5c75bd6
Ming Huang <mingh1002@gmail.com>
2021-12-21 16:55:40 -0500

    my step 1

--- config.js
+++ config.js
@@ -16,3 +16,41 @@ twitch.onAuthorized((auth) => {
   userId = auth.userId; 
 });
 
+$(function() {
+  $('#form').submit(function(e) {
+    e.preventDefault();
+    options = [];
+    $('input[type=checkbox]').each(function() {
+      if (this.checked) {
+        var option = $(this).val();
+        options.push(option);
+      }
+    });
+    console.log(options)
+  });
+});
+
+function updateConfig() {
+  twitch.configuration.set('broadcaster', '1', JSON.stringify(options));
+}
+
+twitch.configuration.onChanged(function() {
+  // Checks if configuration is defined
+  if (twitch.configuration.broadcaster) {
+    try {
+      // Parsing the array saved in broadcaster content
+      var config = JSON.parse(twitch.configuration.broadcaster.content);
+      
+      // Checking the content is an object
+      if (typeof config === 'object') {
+        // Updating the value of the options array to be the content from config
+        options = config;
+        updateOptions();
+      } else {
+        console.log('Invalid config');
+      }
+    } catch (e) {
+      console.log('Invalid config');
+    }
+  }
+});
